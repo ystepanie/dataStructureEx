799f7502c88a35beaf56728ebe18c78b073bb6fa
ystepanie <ystepanie@naver.com>
2024-09-12 18:18:19 +0900

    - 이진탐색 트리 검색 추가 - 이진탐색 트리 삭제 추가
    TODO 이진탐색 재귀 계산 테스트

--- src/TreeNode/Dfs.java
+++ src/TreeNode/Dfs.java
@@ -37,7 +37,107 @@ public class Dfs {
 	}
 
 	// 루트 트리에서 검색
-	// public TreeNode FindTreeNode(BinarySearchTree tree, int target) {
-	//
-	// }
+	public TreeNode FindTreeNode(BinarySearchTree tree, int target) {
+		if (tree.root == null) {
+			return null;
+		}
+		return FindValue(tree.root, target);
+	}
+
+	// 최상위 값 넣기
+	public void InsertTreeNode(BinarySearchTree tree, int value) {
+		if (tree.root == null) {
+			tree.root = new TreeNode(value, null, null, null);
+		} else {
+			InsertNode(tree.root, value);
+		}
+	}
+
+	// 값 넣기(중복 비허용)
+	public void InsertNode(TreeNode current, int value) {
+		if (value == current.value) {
+			return;
+		}
+
+		if (value < current.value) {
+			if (current.left != null) {
+				InsertNode(current.left, value);
+			} else {
+				current.left = new TreeNode(value, null, null, current);
+			}
+		} else {
+			if (current.right != null) {
+				InsertNode(current.right, value);
+			} else {
+				current.right = new TreeNode(value, null, null, current);
+			}
+		}
+
+	}
+
+	// 값 삭제하기
+	public void RemoveTreeNode(BinarySearchTree tree, TreeNode node) {
+		if (tree.root == null || node == null)
+			return;
+
+		// 리프노드 제거
+		if (node.left == null && node.right == null) {
+			if (node.parent == null) {
+				tree.root = null;
+			} else if (node.parent.left == node) {
+				node.parent.left = null;
+			} else {
+				node.parent.right = null;
+			}
+			return;
+		}
+
+		// 자식이 하나인 노드 제거
+		if (node.left == null || node.right == null) {
+			TreeNode child = node.left;
+			if (node.left == null) {
+				child = node.right;
+			}
+
+			child.parent = node.parent;
+			// 부모노드가 없으면 남은 자식노드가 최상위가 된다.
+			if (node.parent == null) {
+				tree.root = child;
+			} else if (node.parent.left == node) {
+				node.parent.left = child;
+			} else {
+				node.parent.right = child;
+			}
+			return;
+		}
+
+		// 자식이 두개인 노드 제거
+		if (node.left != null && node.right != null) {
+			//후보자 찾기
+			TreeNode successor = node.right;
+			while (successor.left != null) {
+				successor = successor.left;
+			}
+			RemoveTreeNode(tree, successor);
+
+			//후보자를 삭제한 노드와 교체
+			if (node.parent == null) {
+				tree.root = successor;
+			} else if (node.parent.left == node) {
+				node.parent.left = successor;
+			} else {
+				node.parent.right = successor;
+			}
+
+			// 후보자와 후보자 자식간의 노드 연결
+			successor.left = node.left;
+			node.left.parent = successor;
+
+			successor.right = node.right;
+			// 후보자가 리프노드일 경우 순서가 바뀌면 오른쪽은 없는 경우가 있을 수 있다.
+			if (node.right != null) {
+				node.right.parent = successor;
+			}
+		}
+	}
 }
